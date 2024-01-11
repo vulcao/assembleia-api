032d40b7fc55c5632018c00e743f9cae7f6a9656
vulcao <vulcao@gmail.com>
2024-01-11 17:25:22 -0300

    aula 40 concluída próxima é a 41

--- src/voto/associado/associado.entity.ts
+++ src/voto/associado/associado.entity.ts
@@ -1,1 +1,11 @@
-export class Associado {}
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+
+@Entity()
+export class Associado {
+  
+  @PrimaryGeneratedColumn()
+  id?: string;
+
+  @Column()
+  cpf: string;
+}
--- src/voto/voto.entity.ts
+++ src/voto/voto.entity.ts
@@ -1,6 +1,26 @@
-import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn } from 'typeorm'
+import { Entity,PrimaryGeneratedColumn,Column, JoinColumn, ManyToOne } from 'typeorm'
+import { Pauta } from 'src/pautas/pauta.entity';
+import {Associado} from './associado/associado.entity';
 
 @Entity()
 export class Voto {
   
+  @PrimaryGeneratedColumn()
+  id?: string;
+
+  @ManyToOne( () => Pauta )
+  @JoinColumn({ name: 'id_pauta' })
+  pauta: Pauta;
+
+  @ManyToOne( () => Associado )
+  @JoinColumn({ name: 'id_associado' })
+  associado: Associado;
+
+  @Column({ name: 'voto' })
+  opcaoVoto: OpcaoVoto;
+}
+
+export enum OpcaoVoto {
+  SIM = 'SIM',
+  NAO = 'NAO'
 }
